3e5dc9859975804e427c1005eb2d987233ac2a9b
Tyson Lackey <lackeyt90@gmail.com>
2020-10-07 13:04:45 -0700

    add coin values, score tracker, countdown time

---
+++ BomberMan/Assets/TimeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+  public int countdownTime;
+  public Text countdownText;
+
+  private void Start()
+  {
+    StartCoroutine(Countdown());
+  }
+  IEnumerator Countdown()
+  {
+    while(countdownTime >= 0)
+    {
+      countdownText.text = countdownTime.ToString();
+
+      yield return new WaitForSeconds(1f);
+
+      countdownTime--;
+    }
+  }
+}
